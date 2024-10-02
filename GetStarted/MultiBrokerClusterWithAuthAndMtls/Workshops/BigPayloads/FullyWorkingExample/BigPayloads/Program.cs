f84a52620144d96c209e4d453bdb9525d07ec807
Simon Randby <Simon.Glisic.Randby@nhn.no>
2024-10-02 09:22:19 +0200

    BigPayloads WS: Clean up logging in Program.cs
    Also remove leftover webserver debugging adjustments

--- GetStarted/MultiBrokerClusterWithAuthAndMtls/Workshops/BigPayloads/FullyWorkingExample/BigPayloads/Program.cs
+++ GetStarted/MultiBrokerClusterWithAuthAndMtls/Workshops/BigPayloads/FullyWorkingExample/BigPayloads/Program.cs
@@ -6,25 +6,24 @@ var builder = WebApplication.CreateSlimBuilder(args);
 builder.Services.AddSingleton<ChunkingProducer>();
 builder.Services.AddHostedService<ChunkedStreamConsumer>();
 
-Console.WriteLine("Registering schemas");
-await KafkaSchemaRegistration.RegisterSchemasAsync();
-Console.WriteLine("Creating topics");
-await KafkaTopicCreation.CreateTopicsAsync();
-Console.WriteLine("Waiting for a couple of seconds so that the Kafka cluster has time to sync topics and things");
-await Task.Delay(TimeSpan.FromSeconds(5));
-
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Ok("Hello World!"));
+app.Logger.LogInformation("Registering schemas");
+await KafkaSchemaRegistration.RegisterSchemasAsync();
+app.Logger.LogInformation("Creating topics");
+await KafkaTopicCreation.CreateTopicsAsync();
+app.Logger.LogInformation("Waiting for a couple of seconds so that the Kafka cluster has time to sync topics and things");
+await Task.Delay(TimeSpan.FromSeconds(5));
+
 
 app.MapGet("/ProduceExamplePayload", async (ChunkingProducer chunkingProducer) =>
 {
-    Console.WriteLine("Received request to produce example payload");
+    app.Logger.LogInformation("Received request to produce example payload");
     var examplePayload = File.ReadAllText("./CatIpsum.txt");
     var examplePayloadBytes = System.Text.Encoding.UTF8.GetBytes(examplePayload);
-    Console.WriteLine("Sending example payload");
+    app.Logger.LogInformation("Sending example payload");
     await chunkingProducer.ProduceChunkedAsync(examplePayloadBytes);
     return Results.Ok($"Example payload produced!");
 });
-// app.Urls.Add("http://*:8080");
+
 app.Run();
