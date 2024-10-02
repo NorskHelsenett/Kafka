7f7579905e52b8b65f595d5675e632f1ad13b933
Simon Randby <Simon.Glisic.Randby@nhn.no>
2024-10-02 09:21:28 +0200

    BigPayloads WS: Remove initial poc code

--- GetStarted/MultiBrokerClusterWithAuthAndMtls/Workshops/BigPayloads/FullyWorkingExample/BigPayloads/Program.cs
+++ GetStarted/MultiBrokerClusterWithAuthAndMtls/Workshops/BigPayloads/FullyWorkingExample/BigPayloads/Program.cs
@@ -1,37 +1,6 @@
 ﻿global using static EnvVarNames;
 using BigPayloads;
 
-// Original POC
-
-// var bigPayload = File.ReadAllText("./CatIpsum.txt");
-// var bigPayloadBytes = System.Text.Encoding.UTF8.GetBytes(bigPayload);
-// var bigPayloadChecksum = Convert.ToHexString(System.Security.Cryptography.SHA256.Create().ComputeHash(bigPayloadBytes));
-// var bytesPerChunk = 128;
-// var payloadChunks = bigPayloadBytes.Chunk(bytesPerChunk).ToArray();
-// var numberOfChunks = payloadChunks.Length;
-//
-// var reconstructedPayloadBytes = new List<byte>();
-// foreach (var chunk in payloadChunks)
-// {
-//     reconstructedPayloadBytes.AddRange(chunk);
-// }
-// var reconstructedBytes = reconstructedPayloadBytes.ToArray();
-// var reconstructedChecksum = Convert.ToHexString(System.Security.Cryptography.SHA256.Create().ComputeHash(reconstructedBytes));
-// var checksumsMatch = reconstructedChecksum == bigPayloadChecksum;
-// var reconstructed = System.Text.Encoding.UTF8.GetString(reconstructedBytes);
-// var reconstructedMatches = reconstructed == bigPayload;
-//
-// Console.WriteLine($"Reconstructed payload:\n\n{reconstructed}\nENDED\n");
-//
-// Console.WriteLine($"Checksums match: {checksumsMatch}");
-// Console.WriteLine($"Payloads match: {reconstructedMatches}");
-// Console.WriteLine($"Original as bytes length (bytes): {bigPayloadBytes.Length}");
-// Console.WriteLine($"Chunk size (bytes): {bytesPerChunk}");
-// Console.WriteLine($"Number of chunks: {numberOfChunks}");
-
-// END Original POC
-// BEGIN New kafkaesque existence
-
 var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.AddSingleton<ChunkingProducer>();
